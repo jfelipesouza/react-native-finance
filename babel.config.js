2def02833f9ac6bec39d9310dc36cfd8d2d0ab97
Felipe Souza <j.felipe.dev.18@gmail.com>
2025-04-03 03:25:37 -0300

    criada integração de pacote de variavel de ambiente

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,16 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['module:@react-native/babel-preset'],
-    plugins: [['react-native-reanimated/plugin']],
+    plugins: [
+      ['react-native-reanimated/plugin'],
+      [
+        'module:react-native-dotenv',
+        {
+          envName: 'APP_ENV',
+          moduleName: '@env',
+          path: '.env',
+        },
+      ],
+    ],
   };
 };
